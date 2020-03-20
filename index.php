73a76d414444596f76eaf1b84791d2121ce37149
Vishal Agarwal <vishal.aug2@gmail.com>
2020-03-20 19:05:03 +0530

    setup

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+/**
+ * Instantiate App
+ *
+ * In order for the factory to work you need to ensure you have installed
+ * a supported PSR-7 implementation of your choice e.g.: Slim PSR-7 and a supported
+ * ServerRequest creator (included with Slim PSR-7)
+ */
+$app = AppFactory::create();
+
+// Add Routing Middleware
+$app->addRoutingMiddleware();
+
+/**
+ * Add Error Handling Middleware
+ *
+ * @param bool $displayErrorDetails -> Should be set to false in production
+ * @param bool $logErrors -> Parameter is passed to the default ErrorHandler
+ * @param bool $logErrorDetails -> Display error details in error log
+ * which can be replaced by a callable of your choice.
+ 
+ * Note: This middleware should be added last. It will not handle any exceptions/errors
+ * for middleware added after it.
+ */
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->get('/dev', function (Request $request, Response $response, $args) {
+    $response->getBody()->write('Hello world!');
+    return $response;
+});
+
+
+
+$app->get('/phpinfo', function (Request $request, Response $response, $args) {
+    $response->getBody()->write(phpinfo());
+    return $response;   
+});
+
+// Define app routes
+$app->get('/hello/{name}', function (Request $request, Response $response, $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
+
+// Run app
+$app->run();
